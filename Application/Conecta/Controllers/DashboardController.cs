889708255c50cdae68f41ad01055185cb59aab16
mavsters <mavster@fenix-alliance.com>
2018-10-11 09:36:19 -0500

    [Add] New

---
+++ Application/Conecta/Controllers/DashboardController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conectaconlamemoria.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+
+
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            getNameUser();
+            return View();
+        }
+
+        public ActionResult Eventos()
+        {
+            getNameUser();
+            return View();
+        }
+
+        public ActionResult Historia()
+        {
+            getNameUser();
+            return View();
+        }
+        public ActionResult Conecta()
+        {
+            getNameUser();
+            return View();
+        }
+        public ActionResult App()
+        {
+            getNameUser();
+            return View();
+        }
+        public ActionResult Instrucciones()
+        {
+            getNameUser();
+            return View();
+        }
+
+
+        public void getNameUser()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewData["FullName"] = User.Identity.Name;
+            }
+            else
+            {
+                ViewData["FullName"] = "Home page for guest user.";
+            }
+        }
+
+
+        // GET: Dashboard/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Dashboard/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dashboard/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dashboard/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Application/Conecta/Controllers/FaunayFloraController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conectaconlamemoria.Controllers
+{
+    public class FaunayfloraController : Controller
+    {
+        // GET: FaunayFlora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FaunayFlora/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FaunayFlora/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FaunayFlora/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FaunayFlora/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FaunayFlora/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FaunayFlora/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FaunayFlora/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
