3dc722b8b963c80bd77bf86631a992b14e0590dc
mavsters <mavster@fenix-alliance.com>
2018-10-13 03:07:45 -0500

    [Fix] Layout Web

--- Application/Conecta/wwwroot/js/site.js
+++ Application/Conecta/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿$(function () { materialKitDemo.initContactUs2Map(); });
+﻿WebFontConfig = { google: { families: ['Roboto|Roboto+Slab|Material+Icons'] } };
+(function () {
+    var wf = document.createElement('script');
+    wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wf, s);
+    //Icons
+    var host = window.location.host;
+    $('head').append('<link rel="stylesheet" href="//' + host +'/css/font.css" type="text/css" />');
+})();
