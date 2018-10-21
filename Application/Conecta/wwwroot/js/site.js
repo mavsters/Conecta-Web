cd70a04a085075d24ec9ebfce72b1d457830b95c
mavsters <mavster@fenix-alliance.com>
2018-10-20 21:54:52 -0500

    [Add] New Content Memory Grapsh

--- Application/Conecta/wwwroot/js/site.js
+++ Application/Conecta/wwwroot/js/site.js
@@ -18,3 +18,9 @@ $(function () {
         document.getElementById($(this).attr("id")).innerHTML = ytvideo;
     });
 });
+function showfile(content, url) {
+    var urlMain = "/" + url;
+    //<object data="~/docs/memoria-grafica/julio/mattasur-conecta.pdf" type='application/pdf' frameborder='0' style='overflow: hidden;position: relative;overflow-x: hidden;overflow-y: hidden;height: -webkit-fill-available;width: 100%;' height='100%' width='100%'></object>
+    var contentTemp = "<object data='" + urlMain +"' type='application/pdf' frameborder='0' style='overflow: hidden;position: relative;overflow-x: hidden;overflow-y: hidden;height: -webkit-fill-available;width: 100%;' height='100%' width='100%'></object>";
+    document.getElementById("content-" + content).innerHTML = contentTemp;
+}
